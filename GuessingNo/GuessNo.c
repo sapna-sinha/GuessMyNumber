d9723d8b80e18361319d9a881cecb8952676a3f4
Sapna Sinha <66251923+sapna-sinha@users.noreply.github.com>
2021-08-07 20:25:10 +0530

    Update GuessNo.c

--- GuessingNo/GuessNo.c
+++ GuessingNo/GuessNo.c
@@ -3,7 +3,7 @@
 #include<time.h>
 
 int main(){
-    int number, guess, nguesses=1;
+    int number, guess, nguesses=5,count=1;
     srand(time(0));
     number = rand()%100 + 1; // Generates a random number between 1 and 100
     // printf("The number is %d\n", number);
@@ -11,16 +11,22 @@ int main(){
     do{
         printf("Guess the number between 1 to 100\n");
         scanf("%d", &guess);
+         if(nguesses<1){
+            printf("You Loose...Better Luck Next time.");
+            break;
+        }
+        int a= nguesses-1;
         if(guess>number){
-            printf("Lower number please!\n");
+            printf("Lower number please!..%d attempt is left\n",a);
         }
         else if(guess<number){
-            printf("Higher number please!\n");
+            printf("Higher number please!..%d attempt is left\n",a);
         }
         else{
-            printf("You guessed it in %d attempts\n", nguesses);
+            printf("You guessed it in %d attempts\n", count);
         }
-        nguesses++;
+        nguesses--;
+        count++;
     }while(guess!=number);
     
     return 0;
